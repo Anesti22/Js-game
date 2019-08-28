b17785d8bb4ec746e3dc25aadaf5e615c6be7de1
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-08-28 13:48:25 +0200

    Creating Objects Object.create

--- 3-Advanced Js/app.js
+++ 3-Advanced Js/app.js
@@ -1,5 +1,7 @@
 // #1 Function Constructor
 
+/*
+
 var jonh = {
     name: 'John',
     yearOfBirth: 1990,
@@ -61,3 +63,32 @@ var maria = new Njeriu('Maria', 'Soriti', 1988, 25);
 var toto = new Njeriu('Toto', 'Gjikoka', 1988, 29);
 
 nesti.llogaritMoshen();
+
+*/
+
+
+// Object.create
+var personProto = {
+    calculateAge: function (){
+        console.log(2016 - this.yearOfBirth);
+    }
+}
+
+var john = Object.create(personProto);
+
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'Programmer';
+
+var jane = Object.create(personProto,
+    {
+        name: {value: 'Jane'},
+        yearOfBirth: {value: 1988},
+        job:{value: 'Designer'}
+    }
+);
+
+
+
+
+
